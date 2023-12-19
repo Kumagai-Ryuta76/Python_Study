1df510ffcc1695933eb848358a80f7f62b11e995
Kumagai Ryuta <kuma_ryu76@icloud.com>
2023-12-19 19:27:41 +0900

    2023/12/19 First_Commit

--- python_Study/Study1.py
+++ python_Study/Study1.py
@@ -1,1 +1,19 @@
+"""
+print関数　→　画面に出力する関数
+"""
 print('Hello, World!!')
+print('熊谷の学習スタート！')
+print('28歳')
+print('Web開発エンジニア目指して勉強中！')
+# 数値の場合は''は不要
+print(10)
+print(-100)
+# print()の中で計算することも可能
+print(1 + 2) #加算
+print(100 - 50) #減算
+print(10 * 5) #乗算
+print(7 / 4) #除算
+print(7 // 4) #割り算の章
+print(7 % 4) #割り算の余り
+print(2 ** 3) #累乗
+
